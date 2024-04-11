815fd1fb30196d21932e3525fad9e9e978bb7e3a
NataliaIvasiv <143248403+NataliaIvasiv@users.noreply.github.com>
2024-04-12 01:51:12 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = 'https://api.unsplash.com/search/phohtos'; 
+axios.defaults.baseURL = 'https://api.unsplash.com/search/photos'; 
 const fetchApi = async (query, currentPage) => {
    
     const responce = await axios.get(`/?client_id=FTiKXxjaeRfuZ2RTzSQCcKu50g3gL8Gg01hLHqRc9pY&orientation=landscape&query=${query}&per_page=12&page=${currentPage}`)
